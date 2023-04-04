397553c5354f4fa558bd16dbcdf388fce94f5522
apollins <polyastep3@gmail.com>
2023-04-05 01:25:58 +0300

    Add Task List Page and Detail Page.

--- base/views.py
+++ base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
+from django.views.generic.detail import DetailView
 
-def taskList(request): 
-    return HttpResponse("To Do list")
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
